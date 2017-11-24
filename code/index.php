f6059deff780b2125b00895fd497942f7d988d1f
crimson-carnival <vyommaitreya@gmail.com>
2017-11-24 23:09:29 +0530

    Added code editor, made header absolute

---
+++ code/index.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+    <title>Code Editor - idE</title>
+
+    <?php include '../assets/parts/includes.php'; ?>
+</head>
+
+<body>
+    <?php include '../assets/parts/header.php'; ?>
+    <section id="api">
+        <article id="code_wrapper">
+            <textarea id="code">Enter code here</textarea>
+        </article>
+    </section>
+</body>
+
+</html>
+
+<!--
+C
+C#
+C++
+-->
