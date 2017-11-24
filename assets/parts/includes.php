84d6b28d6402c9474f39980660fc95e6ac6c0b07
mridul-arora <500061036@stu.upes.ac.in>
2017-11-24 23:39:49 +0530

    leaderboard

--- assets/parts/includes.php
+++ assets/parts/includes.php
@@ -2,3 +2,4 @@
 <link href="/assets/css/header.css" rel="stylesheet" />
 <link href="/assets/css/register.css" rel="stylesheet" />
 <link href="/assets/css/code.css" rel="stylesheet" />
+<link href="/assets/css/leaderboard.css" rel="stylesheet" />
---
+++ leaderboard/index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Register - Hackathon_IDE</title>
+        <?php include '../assets/parts/includes.php' ?>
+    </head>
+    <body>
+        <?php include '../assets/parts/header.php'; ?>
+
+        <table>
+            <th>S.No.</th>
+            <th>Username</th>
+            <th>Rank</th>
+
+            <tr>1.</tr>
+            <tr></tr>
+            <tr></tr>
+
+        </table>
+    </body>
+</html>
