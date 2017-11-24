afcc345422fe5129de5fb321771c7104249f0bff
crimson-carnival <vyommaitreya@gmail.com>
2017-11-25 02:07:38 +0530

    Right navbar menu

--- assets/parts/header.php
+++ assets/parts/header.php
@@ -1,3 +1,9 @@
 <header id="header">
     <a href="/" id="brand"><strong>idE</strong></a>
+    <div class="right">
+        <ul>
+            <li><a href="logout" class="nav">Logout</a></li>
+            <li><a href="logout" class="nav">Logout</a></li>
+        </ul>
+    </div>
 </header>
