9ec4ac480e04147dcb8b230001179022ae777cb1
crimson-carnival <vyommaitreya@gmail.com>
2017-11-25 02:13:35 +0530

    Logout.php

--- assets/parts/header.php
+++ assets/parts/header.php
@@ -2,8 +2,9 @@
     <a href="/" id="brand"><strong>idE</strong></a>
     <div class="right">
         <ul>
-            <li><a href="logout" class="nav">Logout</a></li>
-            <li><a href="logout" class="nav">Logout</a></li>
+            <li><a href="/code" class="nav">Code</a></li>
+            <li><a href="/leaderboard" class="nav">Leaderboard</a></li>
+            <li><a href="/logout" class="nav">Logout</a></li>
         </ul>
     </div>
 </header>
