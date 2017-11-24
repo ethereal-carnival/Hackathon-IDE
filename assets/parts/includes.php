42c59db7fe40a072d71eb8e5375735e45fa9c2be
mridul-arora <500061036@stu.upes.ac.in>
2017-11-24 21:14:14 +0530

    register.css

--- assets/parts/includes.php
+++ assets/parts/includes.php
@@ -1,1 +1,3 @@
 <link href="/assets/css/header.css" rel="stylesheet" />
+<link href="/assets/css/register.css" rel="stylesheet" />
+
---
+++ register/index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Register - Hackathon_IDE</title>
+        <?php include '../assets/parts/includes.php' ?>
+    </head>
+    <body>
+        <?php include '../assets/parts/header.php'; ?>
+
+        <form id="a" action="">
+  Name:
+  <input placeholder="Name" type="text" name="name" value="">
+  <br>
+  Password:
+  <input placeholder="Password" type="text" name="pwd" value="">
+  <br><br>
+  <input type="submit" value="Submit">
+</form>
+
+    </body>
+</html>
