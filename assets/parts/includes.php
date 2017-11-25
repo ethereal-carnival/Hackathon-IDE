199d40162c173b8b939b5521bb634654d9f2b810
crimson-carnival <vyommaitreya@gmail.com>
2017-11-25 05:42:47 +0530

    Registration - front end done

--- assets/parts/includes.php
+++ assets/parts/includes.php
@@ -1,10 +1,10 @@
 <link href="/assets/css/common.css" rel="stylesheet" />
 <link href="/assets/css/header.css" rel="stylesheet" />
-<link href="/assets/css/register.css" rel="stylesheet" />
 <link href="/assets/css/code.css" rel="stylesheet" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="/assets/css/leaderboard.css" rel="stylesheet" />
 <link href="/assets/css/login.css" rel="stylesheet" />
+<link href="/assets/css/register.css" rel="stylesheet" />
 
 <script src="/assets/javascript/main.js" type="text/javascript"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
--- register/index.php
+++ register/index.php
@@ -1,28 +1,41 @@
-<?php session_start();
-    if(isset($_SESSION['username'])) echo "
-    <html><head><script type='text/javascript'>
-    alert('Please log out before registering as another user');
-    window.location.href = '/';
-    </script></head></html>";
-?>
 <html>
     <head>
         <title>Register - uCode</title>
-        <?php include '../assets/parts/includes.php' ?>
+
+        <?php include '../assets/parts/includes.php'; ?>
     </head>
     <body>
         <?php include '../assets/parts/header.php'; ?>
-<div class="padding">
-       <div class="center">
-     <form action="">
-
-  <input id="name" placeholder="Name" type="text" name="name" value="">
-  <br>
-  <input placeholder="Password" type="text" name="pwd" value="">
-  <br><br>
-  <input id="submit" type="submit" value="Submit">
-           </form>
-        </div>
-</div>
+        <div id="register_wrapper">
+        <center>
+            <section id="register_box">
+                <form action="/assets/functions/register.php" method="post">
+                    <article class="inp-group">
+                        <input class="inp-txt inp-reg" type="text" placeholder="First Name" name="fname" />
+                        <input class="inp-txt inp-reg" type="text" placeholder="Last Name" name="lname" />
+                    </article>
+                    <article class="inp-group">
+                        <input class="inp-txt inp-reg" type="text" placeholder="Middle Name" name="mname" />
+                        <input class="inp-txt inp-reg" type="text" placeholder="SAP ID" name="sap" />
+                    </article>
+                    <article class="inp-group">
+                        <input class="inp-txt inp-reg" type="text" placeholder="Username" name="username" />
+                        <input class="inp-txt inp-reg" type="password" placeholder="Password" name="password" />
+                    </article>
+                    <article class="inp-group">
+                        <input class="inp-txt inp-reg" type="text" placeholder="Email ID (.upes.ac.in)" name="email" />
+                        <input class="inp-txt inp-reg" type="text" placeholder="Contact Number" name="number" />
+                    </article>
+                    <article class="inp-group">
+                        <input class="inp-txt inp-reg" type="text" placeholder="Course" name="course" />
+                        <input class="inp-txt inp-reg" type="number" placeholder="Year" name="Year" />
+                    </article>
+                    <article>
+                        <input type="submit" name="Submit" value="Log in" class="btn-submit"/>
+                    </article>
+                </form>
+            </section>
+        </center>
+    </div>
     </body>
 </html>
